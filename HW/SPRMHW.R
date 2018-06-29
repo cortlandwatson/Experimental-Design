a5f4f8c71ad7861e58685a2aa1a1ca0a27c95ddd
Cortland Watson <wat15027@byui.edu>
2018-06-29 12:55:33 -0600

    update

--- HW/SPRMHW.R
+++ HW/SPRMHW.R
@@ -45,13 +45,29 @@ plot(splitplot,which=1:2)
 hist(splitplot$residuals)
 qqPlot(splitplot$residuals)
 
+#### 4 
+power.anova.test(groups=4, between.var=var(c(21,23,25,27)),within.var=4.7, sig.level=0.05, n=2)
+# 2=.31, 3=.64, 4=.84, 5=.94, 6=.97, 7=.993, 8=.998, 9=.9994, 10=.9998, 11=.99995, 
+# 12=.99998, 13=.999997, 14=.9999994, 15=.9999998, 16=1, 17=1, 18=1, 19=1, 20=1
+x <- c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+y <- c(.31,.64,.84,.94,.97,.993,.998,.9994,.9998,.99995,.99998,.999997,.9999994,.9999998,1,1,1,1,1)
+Power <- data.frame(x,y)
+plot(Power)
+power.anova.test(groups=5, between.var=var(c(0,0,0,0,10)),within.var=11.67^2, sig.level=0.05, n=25)
+
+x <- rep(NA,20)
+for (i in 2:20){
+  x[i] <- power.anova.test(groups=4, between.var=var(c(0,2,4,6)), within.var=4.7^2, sig.level=0.05, n=i)$power
+  
+}
+x
+i
+n <- c(1:20)
+scatterplot(x~n)
 
 
-
-
-
-
-
+###Project
+sample(1:2, 8, replace=TRUE)
 
 
 
